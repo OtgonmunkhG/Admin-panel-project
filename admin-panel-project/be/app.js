459bf4673ee80fdef72c19f7fdee9300d1908c32
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-01-31 21:45:28 +0800

    added with be && fe during to formal standart

---
+++ admin-panel-project/be/app.js
@@ -0,0 +1,17 @@
+console.log("hello my first app")
+
+const express = require("express")
+const cors = require("cors")
+const fs = require("fs")
+
+const app = express()
+
+const PORT = 8080;
+
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
