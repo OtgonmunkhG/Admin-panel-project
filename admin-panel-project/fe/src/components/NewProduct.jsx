a489c711e49ae32c443157c67a1f09c8fa19c459
Morning <morning@mtars-lab2-17s-iMac.local>
2023-02-02 09:24:36 +0800

    update

--- admin-panel-project/fe/src/components/NewProduct.jsx
+++ admin-panel-project/fe/src/components/NewProduct.jsx
@@ -1,119 +1,116 @@
-import React, { useState, useEffect } from 'react'
-import { Button, Box, Grid, TextField, InputLabel, MenuItem, FormControl, Select } from "@mui/material";
-
+import {
+  Button,
+  Box,
+  Grid,
+  TextField,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from "@mui/material";
 
 const NewProduct = () => {
-    const URL = "http://localhost:8080/products"
-    const newProduct = {
-        title: "",
-        subTitle: "",
-        price: "",
-        description: "",
-        color: "",
-    };
-
-    const [color, setColor] = React.useState('');
-    const [product, setProduct] = useState([])
-    const [currentProduct, setCurrentProduct] = useState(newProduct)
-
-    useEffect(() => {
-        fetchAllData();
-    }, [])
-
-    async function fetchAllData() {
-        const FETCHED_DATA = await fetch(URL);
-        const FETCHED_JSON = await FETCHED_DATA.json();
-        setProduct(FETCHED_JSON.data)
-    }
-
-
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const postData = {
-            title: e.target.title.value,
-            subTitle: e.target.subTitle.value,
-            price: e.target.price.value,
-            description: e.target.description.value,
-            color: e.target.color.value,
-        }
-        console.log("data", postData);
-
-        const options = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(postData),
-        }
-
-        const FETCHED_DATA = await fetch(URL, options);
-        const FETCHED_JSON = await FETCHED_DATA.json();
-        setProduct(FETCHED_JSON.data);
-        // setCurrentProduct(newProduct);
-
-    }
-    const handleChange = (event) => {
-        setColor(event.target.value);
-    };
-    return (
-        <Box m="20px" sx={{ width: 550 }}>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <Grid container spacing={4}>
-                        <Grid item xs={12}>
-                            <TextField name='title' placeholder="Enter Title" id="filled-basic" label="Title" variant="filled" color="secondary" required fullWidth />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField name='subTitle' placeholder="Enter Sub Title" id="filled-basic" label="Subtitle" variant="filled" color="secondary" required fullWidth />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField name='price' placeholder="Enter Price" id="filled-number" label="Price" type="number" InputLabelProps={{ shrink: true, }} variant="filled" color="secondary" required fullWidth />
-                        </Grid>
-                        {/* <Grid item xs={12}>
-                            <TextField placeholder="Enter Discount" id="filled-number" label="Discount" type="number" InputLabelProps={{ shrink: true, }} variant="filled" color="secondary" required fullWidth />
-                        </Grid> */}
-                        <Grid item xs={12}>
-                            <TextField name='description' placeholder="Enter Description" id="filled-basic" label="Description" variant="filled" color="secondary" required fullWidth />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-autowidth-label" color="secondary">Color</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-autowidth-label"
-                                    id="demo-simple-select-autowidth"
-                                    value={color}
-                                    onChange={handleChange}
-                                    label="Color"
-                                    variant="filled"
-                                    color="secondary"
-                                    required
-                                    name='color'
-                                >
-                                    <MenuItem value="">
-                                        <em>None</em>
-                                    </MenuItem>
-                                    <MenuItem value={"White"}>White</MenuItem>
-                                    <MenuItem value={"Black"}>Black</MenuItem>
-                                    <MenuItem value={"Red"}>Red</MenuItem>
-                                    <MenuItem value={"Blue"}>Blue</MenuItem>
-                                    <MenuItem value={"Yellow"}>Yellow</MenuItem>
-                                    <MenuItem value={"Orange"}>Orange</MenuItem>
-                                    <MenuItem value={"Green"}>Green</MenuItem>
-                                    <MenuItem value={"Pink"}>Pink</MenuItem>
-                                    <MenuItem value={"Purple"}>Purple</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                        {/* <Grid item xs={12}>
-                            <TextField placeholder="Enter Rating" id="filled-number" label="Rating" type="number" InputLabelProps={{ shrink: true, }} variant="filled" color="secondary" required fullWidth />
-                        </Grid> */}
-                        <Grid item xs={12}>
-                            <Button type="submit" variant="contained" color="success" fullWidth>Submit</Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Box >
-    )
-}
-export default NewProduct
+  return (
+    <Box m="20px" sx={{ width: 550 }}>
+      <div>
+        <form>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <TextField
+                name="title"
+                placeholder="Enter Title"
+                id="filled-basic"
+                label="Title"
+                variant="filled"
+                color="secondary"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="subTitle"
+                placeholder="Enter Sub Title"
+                id="filled-basic"
+                label="Subtitle"
+                variant="filled"
+                color="secondary"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="price"
+                placeholder="Enter Price"
+                id="filled-number"
+                label="Price"
+                type="number"
+                InputLabelProps={{ shrink: true }}
+                variant="filled"
+                color="secondary"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                name="description"
+                placeholder="Enter Description"
+                id="filled-basic"
+                label="Description"
+                variant="filled"
+                color="secondary"
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel
+                  id="demo-simple-select-autowidth-label"
+                  color="secondary"
+                >
+                  Color
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-autowidth-label"
+                  id="demo-simple-select-autowidth"
+                  label="Color"
+                  variant="filled"
+                  color="secondary"
+                  required
+                  name="color"
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={"White"}>White</MenuItem>
+                  <MenuItem value={"Black"}>Black</MenuItem>
+                  <MenuItem value={"Red"}>Red</MenuItem>
+                  <MenuItem value={"Blue"}>Blue</MenuItem>
+                  <MenuItem value={"Yellow"}>Yellow</MenuItem>
+                  <MenuItem value={"Orange"}>Orange</MenuItem>
+                  <MenuItem value={"Green"}>Green</MenuItem>
+                  <MenuItem value={"Pink"}>Pink</MenuItem>
+                  <MenuItem value={"Purple"}>Purple</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="success"
+                fullWidth
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Box>
+  );
+};
+export default NewProduct;
--- admin-panel-project/fe/src/components/Sidebar.jsx
+++ admin-panel-project/fe/src/components/Sidebar.jsx
@@ -7,86 +7,105 @@ import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import PersonIcon from '@mui/icons-material/Person';
+import PersonIcon from "@mui/icons-material/Person";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const drawerWidth = 200; // Sidebar-ын урт хэмжээ.
-const openedMixin = (theme) => ({ // Sidebar нээгдэх үеийн transition area ///
-  width: drawerWidth,                                                       //
-  transition: theme.transitions.create("width", {                           //
-    easing: theme.transitions.easing.sharp,                                 //
-    duration: theme.transitions.duration.enteringScreen,                    //
-  }),                                                                       //
-  overflowX: "hidden",                                                      //
-});   ////////////////////////////////////////////////////////////////////////                                                                     
-const closedMixin = (theme) => ({ // Sidebar Хаагдах үеийн transition area ///
-  transition: theme.transitions.create("width", {                           //
-    easing: theme.transitions.easing.sharp,                                 //
-    duration: theme.transitions.duration.leavingScreen,                     //
-  }),                                                                       //
-  overflowX: "hidden",                                                      //
-  width: `calc(${theme.spacing(7)} + 1px)`,                                 //
-  [theme.breakpoints.up("sm")]: {                                           //
-    width: `calc(${theme.spacing(8)} + 1px)`,                               //
-  },                                                                        //
-});  /////////////////////////////////////////////////////////////////////////
+const drawerWidth = 240;
+const openedMixin = (theme) => ({
+  width: drawerWidth, //
+  transition: theme.transitions.create("width", {
+    //
+    easing: theme.transitions.easing.sharp, //
+    duration: theme.transitions.duration.enteringScreen, //
+  }), //
+  overflowX: "hidden", //
+});
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    //
+    easing: theme.transitions.easing.sharp, //
+    duration: theme.transitions.duration.leavingScreen, //
+  }), //
+  overflowX: "hidden", //
+  width: `calc(${theme.spacing(7)} + 1px)`, //
+  [theme.breakpoints.up("sm")]: {
+    //
+    width: `calc(${theme.spacing(8)} + 1px)`, //
+  }, //
+});
 
-const DrawerHeader = styled("div")(({ theme }) => ({ // Sidebar open and closed arrow icon button area ///
-  display: "flex",                                                                                      //
-  alignItems: "center",                                                                                 //
-  justifyContent: "flex-end",                                                                           //  
-  padding: theme.spacing(0, 1),                                                                         //
-  ...theme.mixins.toolbar,                                                                              //
-}));   ///////////////////////////////////////////////////////////////////////////////////////////////////
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex", //
+  alignItems: "center", //
+  justifyContent: "flex-end", //
+  padding: theme.spacing(0, 1), //
+  ...theme.mixins.toolbar, //
+}));
 
-const Drawer = styled(MuiDrawer, {  // Үндсэн Sidebar Эцэг area ////
-  shouldForwardProp: (prop) => prop !== "open",                   //
-})(({ theme, open }) => ({                                        //
-  width: drawerWidth,                                             //
-  flexShrink: 0,                                                  //
-  whiteSpace: "nowrap",                                           //
-  boxSizing: "border-box",                                        //
-  ...(open && {                                                   //
-    ...openedMixin(theme),                                        //
-    "& .MuiDrawer-paper": openedMixin(theme),                     //
-  }),                                                             //
-  ...(!open && {                                                  //
-    ...closedMixin(theme),                                        //
-    "& .MuiDrawer-paper": closedMixin(theme),                     //
-  }),                                                             //
-}));    ////////////////////////////////////////////////////////////
-
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open", //
+})(({ theme, open }) => ({
+  //
+  width: drawerWidth, //
+  flexShrink: 0, //
+  whiteSpace: "nowrap", //
+  boxSizing: "border-box", //
+  ...(open && {
+    //
+    ...openedMixin(theme), //
+    "& .MuiDrawer-paper": openedMixin(theme), //
+  }), //
+  ...(!open && {
+    //
+    ...closedMixin(theme), //
+    "& .MuiDrawer-paper": closedMixin(theme), //
+  }), //
+}));
 export default function Sidebar() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
-  
+
   return (
-    <Box sx={{ display: "flex", marginTop: '100px' }}>
+    <Box sx={{ display: "flex", marginTop: "100px" }}>
       <CssBaseline />
       <Drawer variant="permanent" open={open}>
         <DrawerHeader>
-          <IconButton onClick={ () => setOpen(!open)}>{theme.direction === "rtl" ? (<ChevronRightIcon />) : (<ChevronLeftIcon />)}</IconButton>
+          <IconButton onClick={() => setOpen(!open)}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
         </DrawerHeader>
         <Divider />
         <List>
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton to={"/user"}> 
-                <ListItemIcon><PersonIcon/></ListItemIcon>
-                <ListItemText primary="User" sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding sx={{ display: "block" }}>
-              <ListItemButton to={"/product"}>
-                <ListItemIcon><ShoppingCartIcon/></ListItemIcon>
-                <ListItemText primary="E-commerce" sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
+          <ListItem disablePadding sx={{ display: "block" }}>
+            <ListItemButton to={"/user"}>
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="User" sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{ display: "block" }}>
+            <ListItemButton to={"/product"}>
+              <ListItemIcon>
+                <ShoppingCartIcon />
+              </ListItemIcon>
+              <ListItemText
+                primary="E-commerce"
+                sx={{ opacity: open ? 1 : 0 }}
+              />
+              <Box></Box>
+            </ListItemButton>
+          </ListItem>
         </List>
         <Divider />
       </Drawer>
